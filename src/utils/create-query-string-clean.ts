265d8ebbcb765147152b66ad88e30a71128d1aec
Felippe Santana <felippefsantana02@gmail.com>
2025-07-26 20:44:06 -0300

    feat: verify if query params is null

--- src/utils/create-query-string-clean.ts
+++ src/utils/create-query-string-clean.ts
@@ -2,7 +2,9 @@ export function createQueryStringClean<T extends Record<string, unknown>>(
   params: T,
 ) {
   const filtered = Object.entries(params)
-    .filter(([, value]) => value !== undefined && value !== '')
+    .filter(
+      ([, value]) => value !== undefined && value !== '' && value !== null,
+    )
     .reduce((acc, [key, value]) => ({ ...acc, [key]: value }), {})
 
   return new URLSearchParams(filtered).toString()
