0badfcf9df14e7715768a1142c9ef834d7151fda
Felippe Santana <felippefsantana02@gmail.com>
2025-07-30 20:00:55 -0300

    implement refresh token request

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,8 +1,19 @@
-import axios from 'axios'
-import { getToken } from '../utils/sessionMethods'
+import axios, { AxiosError } from 'axios'
+import { getToken, removeTokens, storeTokens } from '../utils/sessionMethods'
 import { env } from '@/config/env'
 
 const baseUrl = env.VITE_API_BASE_URL || 'http://localhost:3333'
+
+let isRefreshing = false
+let failedRequestsQueue: Array<{
+  onSuccess: (token: string) => void
+  onFailure: (error: AxiosError) => void
+}> = []
+
+const headers = {
+  Accept: 'application/json',
+  'Content-Type': 'application/json',
+}
 
 export const api = axios.create({
   baseURL: baseUrl,
@@ -20,5 +31,62 @@ api.interceptors.request.use(
   },
   (err) => {
     return Promise.reject(err)
+  },
+)
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error: AxiosError) => {
+    const originalRequest = error.config
+
+    if (originalRequest && error.response?.status === 401) {
+      if (!isRefreshing) {
+        isRefreshing = true
+
+        axios
+          .post('/refresh', undefined, {
+            baseURL: baseUrl,
+            withCredentials: true,
+            headers,
+          })
+          .then((response) => {
+            const { access_token } = response.data
+
+            storeTokens(access_token)
+            api.defaults.headers['Authorization'] = `Bearer ${access_token}`
+
+            failedRequestsQueue.forEach((request) => {
+              request.onSuccess(access_token)
+            })
+            failedRequestsQueue = []
+          })
+          .catch((err: AxiosError) => {
+            failedRequestsQueue.forEach((request) => {
+              request.onFailure(err)
+            })
+            failedRequestsQueue = []
+
+            removeTokens()
+            window.location.href = '/sign-in'
+          })
+          .finally(() => {
+            isRefreshing = false
+          })
+      }
+
+      return new Promise((resolve, reject) => {
+        failedRequestsQueue.push({
+          onSuccess: (accessToken) => {
+            originalRequest.headers.Authorization = `Bearer ${accessToken}`
+            resolve(api(originalRequest))
+          },
+          onFailure: (err) => {
+            reject(err)
+          },
+        })
+      })
+    }
+
+    return Promise.reject(error)
   },
 )
--- src/utils/sessionMethods.ts
+++ src/utils/sessionMethods.ts
@@ -1,11 +1,11 @@
 export function storeTokens(token: string) {
-  sessionStorage.setItem("@docsan_token", token);
+  localStorage.setItem('@docsan_token', token)
 }
 
 export function removeTokens() {
-  sessionStorage.removeItem("@docsan_token");
+  localStorage.removeItem('@docsan_token')
 }
 
 export function getToken() {
-  return sessionStorage.getItem("@docsan_token");
+  return localStorage.getItem('@docsan_token')
 }
