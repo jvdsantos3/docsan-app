8d20b0d9cc9371f65014e375d3755bb326a09dee
joaovitornunes09 <jvitornunes09@gmail.com>
2025-08-09 15:29:53 -0300

    feat: implement create branch activity dialog and associated request type

--- src/components/dialogs/create-branch-activity-dialog.tsx
+++ src/components/dialogs/create-branch-activity-dialog.tsx
@@ -5,12 +5,12 @@ import {
   DialogHeader,
   DialogTitle,
 } from '@/components/ui/dialog'
-// import { useCreateBranchActivity } from '@/http/use-create-branch-activity'
-// import type { CreateBranchActivityRequest } from '@/types/http/create-branch-activity-request'
-// import { toast } from 'sonner'
-// import { useAuth } from '@/hooks/use-auth'
-// import { useProfile } from '@/http/use-profile'
-// import { BranchActivityForm } from '../forms/branch-activity-form'
+import { useCreateBranchActivity } from '@/http/use-create-branch-activity'
+import type { CreateBranchActivityRequest } from '@/types/http/create-branch-activity-request'
+import { toast } from 'sonner'
+import { useAuth } from '@/hooks/use-auth'
+import { useProfile } from '@/http/use-profile'
+import { BranchActivityForm } from '../forms/branch-activity-form'
 
 type CreateBranchActivityDialogProps = {
   open: boolean
@@ -21,40 +21,40 @@ export const CreateBranchActivityDialog = ({
   open,
   onOpenChange,
 }: CreateBranchActivityDialogProps) => {
-  // const { user } = useAuth()
-  // const { data: profile } = useProfile({ enabled: !!user })
-  // const { mutateAsync: createBranchActivity, error: createError } =
-  //   useCreateBranchActivity()
+  const { user } = useAuth()
+  const { data: profile } = useProfile({ enabled: !!user })
+  const { mutateAsync: createBranchActivity, error: createError } =
+    useCreateBranchActivity()
 
-  // const handleCreateBranchActivity = async (
-  //   data: CreateBranchActivityRequest,
-  // ) => {
-  //   const companyId = profile?.user?.owner?.companyId
+  const handleCreateBranchActivity = async (
+    data: CreateBranchActivityRequest,
+  ) => {
+    const companyId = profile?.user?.owner?.companyId
 
-  //   if (!companyId) {
-  //     return
-  //   }
+    if (!companyId) {
+      return
+    }
 
-  //   await createBranchActivity({
-  //     companyId,
-  //     data: {
-  //       name: data.name,
-  //     },
-  //   })
-  //   toast.success('Área de atuação criada com sucesso!', {
-  //     dismissible: true,
-  //     duration: 5000,
-  //     richColors: true,
-  //   })
-  //   onOpenChange(false)
-  // }
+    await createBranchActivity({
+      companyId,
+      data: {
+        name: data.name,
+      },
+    })
+    toast.success('Área de atuação criada com sucesso!', {
+      dismissible: true,
+      duration: 5000,
+      richColors: true,
+    })
+    onOpenChange(false)
+  }
 
-  // if (createError) {
-  //   toast.error('Erro ao criar área de atuação. Tente novamente.', {
-  //     dismissible: true,
-  //     duration: 5000,
-  //   })
-  // }
+  if (createError) {
+    toast.error('Erro ao criar área de atuação. Tente novamente.', {
+      dismissible: true,
+      duration: 5000,
+    })
+  }
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
@@ -68,10 +68,10 @@ export const CreateBranchActivityDialog = ({
         </DialogHeader>
 
         <div>
-          {/* <BranchActivityForm
+          <BranchActivityForm
             onCancel={() => onOpenChange(false)}
             onSubmit={handleCreateBranchActivity}
-          /> */}
+          /> 
         </div>
       </DialogContent>
     </Dialog>
--- src/http/use-create-branch-activity.ts
+++ src/http/use-create-branch-activity.ts
@@ -1,25 +1,25 @@
 // import { api } from '@/lib/axios'
-// import { useMutation, useQueryClient } from '@tanstack/react-query'
-// import type { CreateBranchActivityRequest } from '@/types/http/create-branch-activity-request'
-// import type { Company } from '@/types/company'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import type { CreateBranchActivityRequest } from '@/types/http/create-branch-activity-request'
+import type { Company } from '@/types/company'
 
 export function useCreateBranchActivity() {
-  // const queryClient = useQueryClient()
+  const queryClient = useQueryClient()
 
-  // return useMutation({
-  //   mutationFn: async ({
-  //     companyId,
-  //     data,
-  //   }: {
-  //     companyId: Company['id']
-  //     data: CreateBranchActivityRequest
-  //   }) => {
-  //     console.log(companyId, data)
-  //     return true
-  //     // await api.post(`/company/${companyId}/document-types`, data)
-  //   },
-  //   onSuccess: () => {
-  //     queryClient.invalidateQueries({ queryKey: ['get-branches-activity'] })
-  //   },
-  // })
+  return useMutation({
+    mutationFn: async ({
+      companyId,
+      data,
+    }: {
+      companyId: Company['id']
+      data: CreateBranchActivityRequest
+    }) => {
+      console.log(companyId, data)
+      return true
+      // await api.post(`/company/${companyId}/document-types`, data)
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['get-branches-activity'] })
+    },
+  })
 }
