cf0b2f7768e8a9e1efb13d9f20fb790b5262ab97
Felippe Santana <felippefsantana02@gmail.com>
2025-08-10 16:40:36 -0300

    feat: test

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -1,8 +1,46 @@
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
+import { useAuth } from '@/hooks/use-auth'
+import { api } from '@/lib/axios'
 import { cn } from '@/lib/utils'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Loader2 } from 'lucide-react'
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+const schema = z.object({
+  prompt: z.string().trim(),
+})
+
+type ChatSchema = z.infer<typeof schema>
 
 export const Chat = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<ChatSchema>({
+    resolver: zodResolver(schema),
+  })
+  const { isAuthenticated } = useAuth()
+  const [messages, setMessages] = useState<string[]>([])
+
+  const sendMessage = async (prompt: string) => {
+    if (!prompt) return
+
+    const response = await api.post(
+      isAuthenticated ? '/chat/questions/private' : '/chat/questions',
+      { prompt },
+    )
+
+    console.log(response.data)
+  }
+
+  const onSubmit = ({ prompt }: ChatSchema) => {
+    sendMessage(prompt)
+  }
+
   return (
     <div className="space-y-6 py-6 h-full">
       <div className="bg-white rounded-2xl h-full px-4 md:px-8 lg:px-12 py-6 md:py-10 lg:py-12 flex flex-col justify-end">
@@ -36,19 +74,27 @@ export const Chat = () => {
         </div>
 
         <div>
-          <div className="relative w-full">
-            <Input
-              placeholder="Digite aqui a sua dúvida..."
-              className={cn(
-                'h-[3.5rem] w-full pl-4 pr-36 py-2 border rounded-lg text-gray-800 text-body-sm',
-                'focus-visible:border-blue-500 focus-visible:ring-2 focus-visible:ring-blue-200/50',
-              )}
-            />
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="relative w-full">
+              <Input
+                {...register('prompt')}
+                placeholder="Digite aqui a sua dúvida..."
+                className={cn(
+                  'h-[3.5rem] w-full pl-4 pr-36 py-2 border rounded-lg text-gray-800 text-body-sm',
+                  'focus-visible:border-blue-500 focus-visible:ring-2 focus-visible:ring-blue-200/50',
+                )}
+              />
 
-            <Button className="absolute right-4 top-[50%] -translate-y-[50%] text-white rounded-r-lg hover:bg-blue-600 transition-colors">
-              Perguntar
-            </Button>
-          </div>
+              <Button
+                type="submit"
+                className="absolute right-4 top-[50%] -translate-y-[50%] text-white rounded-r-lg hover:bg-blue-600 transition-colors"
+                disabled={isSubmitting}
+              >
+                {isSubmitting && <Loader2 className="animate-spin" />}
+                Perguntar
+              </Button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
