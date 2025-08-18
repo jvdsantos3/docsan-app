bfe620ca1053b430db7454c89917d0ff163e6f90
joaovitornunes09 <jvitornunes09@gmail.com>
2025-08-18 18:25:01 -0300

    refactor(auth-layout): comment out unused authentication logic and imports

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,26 +1,26 @@
-import { useAuth } from '@/hooks/use-auth'
-import { useEffect } from 'react'
-import { Outlet, useNavigate } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
+// import { useAuth } from '@/hooks/use-auth'
+// import { useEffect } from 'react'
 
 export const AuthLayout = () => {
-  const navigate = useNavigate()
-  const { isAuthenticated, user } = useAuth()
+  // const navigate = useNavigate()
+  // const { isAuthenticated, user } = useAuth()
 
-  useEffect(() => {
-    if (isAuthenticated && user) {
-      let navigateTo = ''
+  // useEffect(() => {
+  //   if (isAuthenticated && user) {
+  //     let navigateTo = ''
 
-      if (user.role === 'ADMIN') {
-        navigateTo = '/admin/cnae'
-      } else if (user.role === 'OWNER') {
-        navigateTo = '/services'
-      } else {
-        navigateTo = '/documents'
-      }
+  //     if (user.role === 'ADMIN') {
+  //       navigateTo = '/admin/cnae'
+  //     } else if (user.role === 'OWNER') {
+  //       navigateTo = '/services'
+  //     } else {
+  //       navigateTo = '/documents'
+  //     }
 
-      navigate(navigateTo, { replace: true })
-    }
-  }, [isAuthenticated, user, navigate])
+  //     navigate(navigateTo, { replace: true })
+  //   }
+  // }, [isAuthenticated, user, navigate])
 
   return (
     <main className="min-h-screen bg-[url(/background-image.png)] bg-repeat flex justify-center items-center">
